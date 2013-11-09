f65d69bd78571d7f7c0182c0ddf1498c638df3d1
Gregory R. Everitt <fake@fake.com>
2013-11-09 17:59:03 -0500

    Application now running on CodeIgniter

---
+++ application/views/main.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Still A Better Love Story Than X</title>
+		<link rel="stylesheet" media="screen" href="default.css" type="text/css"/>
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width">
+	<!--[if lt IE 9]>
+	<script src="dist/html5shiv.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<h1>Still a better love story than <span id="title"><?=$title?></span></h1>
+	<p>
+	<a href="">Another?</a>
+	</p>
+</body>
+</html>
