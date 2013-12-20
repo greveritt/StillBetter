07382cbab2901cc8f5b31e9dc741b550d517c7dc
Gregory R. Everitt <fake@fake.com>
2013-12-20 13:45:37 -0500

    TMDb access is now handled by a model and not the main controller, just as the creators of MVC would have intended

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -4,33 +4,8 @@ class Main extends CI_Controller {
 
 	public function index()
 	{
-		// Initialize TMDB library and get TMDB object
-		require_once($_SERVER['DOCUMENT_ROOT'].'/stillbetter/application/libraries/TMDb.php');
-
-		// Get the API key from an external, one-line text file
-		$keyFile = file($_SERVER['DOCUMENT_ROOT'].'/stillbetter/application/libraries/TMDb_API_key.inc.php');
-		$apiKey = trim($keyFile[0]);
-		// If HTTPS is not being used...
-		if (empty($_SERVER['HTTPS'])) {
-			$tmdb = new TMDb($apiKey);
-		}
-		else {
-			$tmdb = new TMDb($apiKey, 'en', FALSE, TMDb::API_SCHEME_SSL);
-		}
-
-		// Gets basic information on the Romance genre, such as number of pages. Also acquires contents of first page
-		$genreID = 10749;
-		$romanceDump = $tmdb->getMoviesByGenre($genreID);
-
-		// Now we get vital information like the maximum number of pages, and which title to use (from the chosen page)
-		$pageNum = rand(1, $romanceDump["total_pages"]);
-		$movieNum = rand(1, count($romanceDump["results"])) - 1;
-
-		$pageContents = $tmdb->getMoviesByGenre($genreID, $pageNum);
-
-		$title = $pageContents["results"][$movieNum]["title"];
-		$data['title'] = $title;
-
+		$this->load->model('TmdbModel');
+		$data['title'] = $this->TmdbModel->getTitle();
 		$this->load->view('main', $data);
 	}
 }
